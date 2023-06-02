7547c9e9591c421027647bd6fa4e4a91a9fe42da
drmiltonc <92145221+drmiltonc@users.noreply.github.com>
2023-06-02 10:26:28 -0500

    add images in assets and pubspec

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_config/flutter_config.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'app.dart';
 
-void main() {
-  runApp(const MainApp());
-}
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await FlutterConfig.loadEnvVariables();
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
-    );
-  }
+  runApp(const ProviderScope(child: MainApp()));
 }
